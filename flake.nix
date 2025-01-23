a0d53a156358f238150b34cb18a71d2d6790856e
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-01-23 11:38:37 -0300

    Add description

--- flake.nix
+++ flake.nix
@@ -1,4 +1,6 @@
 {
+  description = "Bun and flakes example package";
+
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -8,9 +10,31 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = (import (nixpkgs) { inherit system; });
+
+        helloBun = pkgs.stdenv.mkDerivation {
+          pname = "hello-bun";
+          version = "1.0.0";
+          src = ./.;
+          dontFixup = true;
+          buildInputs = [ pkgs.bun ];
+          buildPhase = ''
+            bun build --compile hello.ts --outfile hello-bun
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp hello-bun $out/bin/
+          '';
+        };
+
       in {
         devShell = pkgs.mkShell {
-          buildInputs=[ pkgs.bun ];
+          buildInputs = [ pkgs.bun ];
+        };
+
+        # Define the default app to run the binary
+        apps.default = {
+          type = "app";
+          program = "${helloBun}/bin/hello-bun";
         };
       }
     );
